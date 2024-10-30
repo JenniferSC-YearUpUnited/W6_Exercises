e5d014818a25869f8deff03585caa297edb6aa0e
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-29 21:05:51 -0500

    Adding script 1.A, Lab1, Q1.

--- Functions/substrings.py
+++ Functions/substrings.py
@@ -1,13 +1,8 @@
-name = 'Jennifer Santos Custodio'
-# Finding location of the substring I am looking for using the index
-print(name.find('Jennifer'))
-#print(full_name.rfind('Jennifer'))
-print(name.find('Santos Custodio'))
-#print(full_name.rfind('Santos Custodio'))
+full_name = 'Eric Harris'
+# Finding location of the space I am looking for using the index
+print(f'The space in the name is position {full_name.find(" ")}')
 
-full_name = name[0:24]
-first_name = name[0:8]
-last_name = name[9:24]
-print(full_name)
-print(first_name)
-print(last_name)
+#using f string
+print(f'Full name:{full_name}')
+print(f'First name:{full_name[0:4]}')
+print(f'Last Name: {full_name[5:11]}')
