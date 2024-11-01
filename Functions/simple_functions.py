a8f6a4d6db552618f1bf5638d6f032fd033af2d9
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-11-01 18:49:59 -0500

    Adding script 1.C, Lab 1.

---
+++ Functions/simple_functions.py
@@ -0,0 +1,21 @@
+# define function and include three parameters
+def favorite_things(name, favmovie, favmusician):
+    return name, favmovie, favmusician
+
+# call the function with the appropriate arguments   
+print(favorite_things(name='Jennifer',favmovie= 'Crybaby',favmusician= 'HUMBE'))
+
+# define function and display a sentence that describes why you joined this Data Analyst program
+def why_im_here(onereason):
+    return onereason 
+
+# call the function with the appropriate arguments 
+print(why_im_here(onereason='I joined this Data Analyst program to break into tech and help companies make data driven decisions.'))
+
+# define function with two parameters
+def favorite_place(favplace, reason):
+    return favplace, reason
+
+# call the function with the appropriate arguments and call this function twice, using different places each time 
+print(favorite_place(favplace='One of my favorite places to visit is "La Michocana Gourmet."',reason='It\'s an ice cream shop and it has a variety of treats including tortas and agua frescas.'))
+print(favorite_place(favplace='One of my favorite places to visit is my mom\'s house.',reason='She makes my favorite meals when she has time.'))
