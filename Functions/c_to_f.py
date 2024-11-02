253b10943b53414beb3db07c8c0c91b1b9a6532b
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-11-02 16:38:37 -0500

    Adding 1.C, Lab 2, Q2.

---
+++ Functions/c_to_f.py
@@ -0,0 +1,9 @@
+def conv_c_to_f(current_temp_C):
+        fahrenheit = (current_temp_C * 9/5) + 32 
+        return round(fahrenheit)
+print(conv_c_to_f(100))
+print(conv_c_to_f(45))
+print(conv_c_to_f(19))
+print(conv_c_to_f(0))
+print(conv_c_to_f(-7))
+print(conv_c_to_f(-40))
