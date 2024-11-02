f2c3fedcea98803a50de7ca3371080689715ec35
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-11-02 16:33:04 -0500

    Updating script  1.C, Lab 3.

--- Functions/f_to_c.py
+++ Functions/f_to_c.py
@@ -1,6 +1,8 @@
 def conv_f_to_c(current_temp_F):
         celsius = (current_temp_F - 32) * 5 / 9 
-        return round(celsius)
+        return f'''
+current_temp_F = {current_temp_F}
+current_temp_C= {round(celsius)}'''
 print(conv_f_to_c(212))
 print(conv_f_to_c(90))
 print(conv_f_to_c(72))
