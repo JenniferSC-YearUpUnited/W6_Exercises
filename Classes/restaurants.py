a14b69721ba7c95de61bdc20cf811d199093e42f
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-11-04 11:57:41 -0600

    Adding script 2.A Lab 1.

--- Classes/restaurants.py
+++ Classes/restaurants.py
@@ -1,24 +1,24 @@
-class Restaurant:
-    '''varieties of restaurants'''
-    
-    def _init_(self, rest_name, food_type):
+# got error message as restaurants did not take any arguements bc i didn't include to underscores for function init. Should be two underscores in front and before init. 
+class Restaurants:
+    '''varieties of restaurants'''  ## including a docstring below the class name to describe the purpose of the class
+    def __init__(self, rest_name, food_type):  # should store two instance variables
         self.rest_name = rest_name
         self.food_type = food_type
 
-    def describe_rest(self):
+    def describe_rest(self): # add two methods to the class this is one and print an output
         print(f'{self.rest_name} serves {self.food_type}')
 
-    def rest_open(self):
-        print(f'{self.rest_name} is open ')        
+    def rest_open(self): # add two methods to the class this is two and print an output
+        print(f'{self.rest_name} is open ')
 
-bubger_kirg = Restaurant('whoopers','burgers')
-donkin_donnts = Restaurant('hot coffe','coffee')
-tacobaco = Restaurant('soft taco', 'tacos')
+# create three instances of the class for different types of restaurant 
+bk= Restaurants('bubger king','burgers')
+dd= Restaurants('donkin donnts','coffee')
+tb= Restaurants('taco baco', 'tacos')
 
-print(f'The resturant I like is {bubger_kirg.rest_name}')
-print(f'{bubger_kirg.rest_name} is {bubger_kirg.food_type}')
-bubger_kirg.rest_open()
-
+#call on each instance
+dd.describe_rest()
+dd.rest_open()
 
         
 
