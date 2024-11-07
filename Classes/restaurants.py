e483bc7d1cf357edccb829701bc70be20d2c4bd3
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-11-06 21:34:24 -0600

    Adding 2.A lab 1.

--- Classes/restaurants.py
+++ Classes/restaurants.py
@@ -1,5 +1,4 @@
-# got error message as restaurants did not take any arguements bc i didn't include to underscores for function init. Should be two underscores in front and before init. 
-class Restaurants:
+ class Restaurants:
     '''Summarize a restaurant'''  ## including a docstring below the class name to describe the purpose of the class
     def __init__(self, rest_name, food_type):  # should store two instance variables
         self.rest_name = rest_name
